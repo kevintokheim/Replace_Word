8622cd9022af0b0db6bb30e26644d1c9d76044b5
Kevin Tokheim <katokh89@gmail.com>
2015-09-11 11:22:23 -0700

    add functions and passing tests for wordFinder and wordReplacer.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,30 @@
-var inputString = function(string){
+var inputString = function(string) {
 
   var userString = string;
 
   return userString;
+
 };
 
-var wordFinder = function(word){
 
-   
+var wordFinder = function(string, word) {
+
+  var userString = string;
+
+  var userWord = word;
+
+  for (var i = 0; i < userString.length; i++) {
+    var wordSearch = string.search(userWord);
+    return userWord;
+  }
+
+}
+
+
+var wordReplacer = function(string, word, replacement) {
+
+  var wordReplace = string.replace(word, replacement);
+
+  return wordReplace;
 
 };
--- specs/specs.js
+++ specs/specs.js
@@ -1,11 +1,17 @@
 describe('inputString', function() {
   it('returns a string', function(){
-    expect(wordFinder('hello world')).to.equal('hello world');
+    expect(inputString('hello world')).to.equal('hello world');
   });
 });
 
-describe('wordFinder', function() {
-  it('allows the user to choose a specific word', function(){
-    expect(wordFinder('world')).to.equal('world');
+describe('wordFinder', function(){
+  it('finds a word within a string', function() {
+    expect(wordFinder('hello world', 'world')).to.equal('world');
+  });
+});
+
+describe('wordReplacer', function() {
+  it('replaces one word with another', function(){
+    expect(wordReplacer('hello world', 'world', 'universe')).to.equal('hello universe');
   });
 });
